d26b97e8015c97fc5d9522388f4439f20c2d586b
yhmbsy <736215675@qq.com>
2026-04-15 20:32:58 +0800

    Add files via upload

---
+++ skeleton/alu.v
@@ -0,0 +1,16 @@
+`timescale 10 ns / 1 ns
+
+`define DATA_WIDTH 32
+
+module alu(
+	input  [`DATA_WIDTH - 1:0]  A,
+	input  [`DATA_WIDTH - 1:0]  B,
+	input  [              2:0]  ALUop,
+	output                      Overflow,
+	output                      CarryOut,
+	output                      Zero,
+	output [`DATA_WIDTH - 1:0]  Result
+);
+	// TODO: Please add your logic design here
+
+endmodule
---
+++ skeleton/reg_file.v
@@ -0,0 +1,19 @@
+`timescale 10 ns / 1 ns
+
+`define DATA_WIDTH 32
+`define ADDR_WIDTH 5
+
+module reg_file(
+	input                       clk,
+	input  [`ADDR_WIDTH - 1:0]  waddr,
+	input  [`ADDR_WIDTH - 1:0]  raddr1,
+	input  [`ADDR_WIDTH - 1:0]  raddr2,
+	input                       wen,
+	input  [`DATA_WIDTH - 1:0]  wdata,
+	output [`DATA_WIDTH - 1:0]  rdata1,
+	output [`DATA_WIDTH - 1:0]  rdata2
+);
+
+	// TODO: Please add your logic design here
+	
+endmodule
